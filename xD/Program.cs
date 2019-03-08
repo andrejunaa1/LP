ad1251588e0ad6c2b33ec7f8d6c8f95bc0e137e8
andrejunaa1 <cectricampeao@hotmail.com>
2019-03-08 15:59:47 +0000

    first commit

---
+++ xD/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "XDXD";
+            string b = @"""to o""";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            string c = "Hello" + " " + "Jr";
+            Console.WriteLine(c);
+
+            string d = $"{5} mais{3} é igual a {5 + 3}";
+            Console.WriteLine(d);
+
+        }
+    }
+}
